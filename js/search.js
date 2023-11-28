6b33db04a2cb56b29edd50549c82a0a1a7234cf4
Kuryleva Anastasia <knastya76@yandex.ru>
2023-11-28 22:15:04 +0300

    second lesson

--- js/search.js
+++ js/search.js
@@ -2,11 +2,7 @@ const search = function () {
   const input = document.querySelector(".search-block > input");
   const searchBtn = document.querySelector(".search-block > button");
 
-  //   input.addEventListener("input", (event) => {
-  //     console.log(event.target.value);
-  //   });
-
-  searchBtn.addEventListener("click", (event) => {
+  searchBtn.addEventListener("click", () => {
     console.log(input.value);
   });
 };
